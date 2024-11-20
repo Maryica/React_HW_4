35a32962daeec307604699ae05704e852d10241b
Maria <pavla0806@gmail.com>
2024-11-20 22:48:17 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+
+
+export default App;
